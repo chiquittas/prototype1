aac4aa32a6fd920819ef12c2e6d2147ca6e55906
chiquittas <chiquittas@gmail.com>
2016-10-16 15:34:05 +0200

    Initial Commit

---
+++ prototype1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prototype1
+//
+//  Created by Yulia B on 10/16/16.
+//  Copyright © 2016 Yulia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
